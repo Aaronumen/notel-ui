e69a1d9657db2dd44122681346f110b43e02b923
Aaronumen <65692618+Aaronumen@users.noreply.github.com>
2022-12-21 22:20:51 +0800

    ✨ feat: notel-space add

---
+++ packages/notel-ui/components/notel-space/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="notel-space"></div>
+</template>
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.notel-space {
+  display: flex;
+  gap: 8px 12px;
+}
+</style>
