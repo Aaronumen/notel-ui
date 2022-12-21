6afa8ef0d3f534329d89a6ade5b7eacb7d7ed13d
Aaronumen <65692618+Aaronumen@users.noreply.github.com>
2022-12-21 23:02:00 +0800

    ✨ feat: notel-space add

--- packages/notel-ui/components/notel-space/index.vue
+++ packages/notel-ui/components/notel-space/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="notel-space"></div>
+  <div
+    class="notel-space"
+    :style="{ rowGap: rowGap + 'px', columnGap: columnGap + 'px' }"
+  >
+    <slot></slot>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts" name="notel-space">
+const props = withDefaults(
+  defineProps<{
+    rowGap?: number | string
+    columnGap?: number | string
+  }>(),
+  {
+    rowGap: 8,
+    columnGap: 8
+  }
+)
+</script>
 
 <style lang="less" scoped>
 .notel-space {
   display: flex;
-  gap: 8px 12px;
+  flex-wrap: wrap;
+  row-gap: 8px;
+  column-gap: 8px;
 }
 </style>
