c5705d239754a052aca15b0153144fc41fd0321b
Aaronumen <65692618+Aaronumen@users.noreply.github.com>
2022-12-22 23:48:24 +0800

    💡 refactor: 优化plugins导出代码

--- packages/plugins/index.ts
+++ packages/plugins/index.ts
@@ -1,3 +1,3 @@
-import { viteBuildLogTime } from "./vite-build-time-plugin"
-import { viteAutoName } from "./vite-auto-name-plugin"
-export { viteAutoName, viteBuildLogTime }
+export * from "./vite-plugin-delete"
+export * from "./vite-auto-name-plugin"
+export * from "./vite-build-time-plugin"
