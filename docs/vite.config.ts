4b3103d5acb93342ffc28e281f8a77d2c6844143
sunyi <1076552287@qq.com>
2022-12-22 11:04:06 +0800

    🐛 fix: 修复docs build|删除多余文件及vite配置

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,53 +1,7 @@
 import { defineConfig } from "vite"
-import AutoImport from "unplugin-auto-import/vite"
-import path from "path"
 import vueJsx from "@vitejs/plugin-vue-jsx"
-import autoprefixer from 'autoprefixer'
-import {viteAutoName,viteBuildLogTime} from '@notel/plugins'
-import postCssPxToRem from 'postcss-pxtorem'
+import { viteBuildLogTime } from "@notel/plugins"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    viteAutoName(),
-    viteBuildLogTime(),
-    vueJsx({}),
-    AutoImport({
-      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
-      imports: [
-        // 插件预设支持导入的api
-        "vue",
-      ],
-      eslintrc: {
-        enabled: true, // Default `false`
-        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
-        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-      },
-      dts: "./auto-imports.d.ts"
-    }),
-  ],
-  css: {
-    postcss:{
-      plugins:[
-        postCssPxToRem({
-          rootValue: 14,// 1rem 的大小
-          propList: ['*']
-      }),
-        autoprefixer({
-          overrideBrowserslist: [
-            "Android 4.1",
-            "iOS 7.1",
-            "Chrome > 31",
-            "ff > 31",
-        ],
-        grid: true
-      })]
-    },
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          hack: `true; @import (reference) "${path.resolve('./styles/global.less')}";`,
-        },
-      },
-    }
-  },
+  plugins: [vueJsx({})]
 })
